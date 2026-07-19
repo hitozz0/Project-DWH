dba751a00a47c1b9b2f57407a701758764aacbec
Hitozz <118142018+hitozz0@users.noreply.github.com>
2026-07-19 21:40:49 +0800

    feat : add dim table

--- service/etl.py
+++ service/etl.py
@@ -2,10 +2,76 @@ import pandas as pd
 import sqlite3
 
 def run_etl():
+    dim_1()
+    dim_2()
+    dim_3()
     fact_1()
     fact_2()
     fact_3()
     print("--- ETL SUCCESSFUL ---")
+
+def dim_1():
+    staging = './databases/data.db'
+    warehouse = './databases/warehouse.db'
+
+    try:
+        conn_staging = sqlite3.connect(staging)
+        df_staging = pd.read_sql("SELECT * FROM merged_data", conn_staging)
+        conn_staging.close()
+
+        dim_cabang = (
+            df_staging.groupby(['id_cabang', 'nama_cabang']).sum().reset_index()
+        )
+
+        conn_warehouse = sqlite3.connect(warehouse)
+
+        dim_cabang.to_sql("dim_cabang", conn_warehouse, if_exists="replace", index=False)
+
+        conn_warehouse.close()
+    except Exception as e:
+        print(f"error : {e}")
+
+def dim_2():
+    staging = './databases/data.db'
+    warehouse = './databases/warehouse.db'
+
+    try:
+        conn_staging = sqlite3.connect(staging)
+        df_staging = pd.read_sql("SELECT * FROM merged_data", conn_staging)
+        conn_staging.close()
+
+        dim_kategori = (
+            df_staging.groupby(['kategori']).sum().reset_index()
+        )
+
+        conn_warehouse = sqlite3.connect(warehouse)
+
+        dim_kategori.to_sql("dim_kategori", conn_warehouse, if_exists="replace", index=False)
+
+        conn_warehouse.close()
+    except Exception as e:
+        print(f"error : {e}")
+
+def dim_3():
+    staging = './databases/data.db'
+    warehouse = './databases/warehouse.db'
+
+    try:
+        conn_staging = sqlite3.connect(staging)
+        df_staging = pd.read_sql("SELECT * FROM merged_data", conn_staging)
+        conn_staging.close()
+
+        dim_product = (
+            df_staging.groupby(['nama_obat']).sum().reset_index()
+        )
+
+        conn_warehouse = sqlite3.connect(warehouse)
+
+        dim_product.to_sql("dim_product", conn_warehouse, if_exists="replace", index=False)
+
+        conn_warehouse.close()
+    except Exception as e:
+        print(f"error : {e}")
 
 def fact_1():
     staging = './databases/data.db'
